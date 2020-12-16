63e92cc9a9e60fffc11cc0256e70f45eb8c7e8cd
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2020-12-15 23:45:48 -0500

    styles header

--- github-jobs/src/components/Header/Header.js
+++ github-jobs/src/components/Header/Header.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './Header.scss';
+import React from "react";
+import "./Header.scss";
 
 function Header() {
-	return (
-		<div className='header'>
-			<h1>Developer Jobs Via Github API</h1>
-            <section className='links'>
-            <a href="#">Home</a>
-            <a href="https://jobs.github.com/">Github Jobs</a>
-            </section>
-		</div>
-	);
+  return (
+    <div className="header">
+      <h1>Get Hired</h1>
+      <section className="links">
+        <a href="#">Home</a>
+        <a href="https://jobs.github.com/">Github Jobs</a>
+      </section>
+    </div>
+  );
 }
 
 export default Header;
