2fca078eb02b9d47ad2ab7ada4b8f6cc8e52188a
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2020-12-15 23:47:46 -0500

    formats

--- github-jobs/src/App.js
+++ github-jobs/src/App.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import Card from './components/Card/Card';
-import Header from './components/Header/Header';
-import axios from 'axios';
-import './App.scss';
-import Search from './components/Search/Search';
-import {jobData} from './components/data/data'
+import React, { useState, useEffect } from "react";
+import Card from "./components/Card/Card";
+import Header from "./components/Header/Header";
+import axios from "axios";
+import "./App.scss";
+import Search from "./components/Search/Search";
+import { jobData } from "./components/data/data";
 
 function App() {
-	const [ data, setData ] = useState([]);
-    
-    useEffect(() => {
-      setData(jobData)
-        
-    }, [])
+  const [data, setData] = useState([]);
 
-	return (
-		<div className='App'>
-            <Header />
-            <Search/>
-			<Card  setData={setData} data={data} />
-		</div>
-	);
+  useEffect(() => {
+    setData(jobData);
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+      <Card setData={setData} data={data} />
+    </div>
+  );
 }
 
 export default App;
--- github-jobs/src/components/Card/Card.js
+++ github-jobs/src/components/Card/Card.js
@@ -1,27 +1,25 @@
-import React, {useEffect} from 'react';
-import './Card.scss'
+import React, { useEffect } from "react";
+import "./Card.scss";
 
 function Card(props) {
-    const {data} = props
-    
+  const { data } = props;
 
-	return (
-        <div className="card-wrapper">
-        	{data.map((job) => {
-				return (
-					<div className="card">
-						<h1>{job.title}</h1>
-						<p>{job.company}</p>
-						<p>{job.location}</p>
-						<p>Salary:${Math.round(job.salary)}</p>
-						<p>{job.fulltime ? 'Full Time' : 'Part Time'}</p>
-						<p>{job.description}</p>
-					</div>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="card-wrapper">
+      {data.map((job) => {
+        return (
+          <div className="card">
+            <h1>{job.title}</h1>
+            <p>{job.company}</p>
+            <p>{job.location}</p>
+            <p>Salary:${Math.round(job.salary)}</p>
+            <p>{job.fulltime ? "Full Time" : "Part Time"}</p>
+            <p>{job.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Card;
-
